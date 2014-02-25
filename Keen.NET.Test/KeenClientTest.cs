b9cf46c467b563911f33535c37fa1e918957fbb1
Sean Sparkman <sean.sparkman@outlook.com>
2014-02-25 13:26:55 -0600

    adding first test

---
+++ Keen.NET.Test/KeenClientTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Keen.NET.Test
+{
+    [TestFixture]
+    public class KeenClientTest
+    {
+        [Test]
+        public void Constructor()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
