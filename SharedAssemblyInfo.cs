9edfcd460ee1be2ac2211d74bc266c482737c224
Justin <Justin.Mason@outlook.com>
2017-02-22 04:27:23 -0500

    Shared Assembly attributes: - Move version-specific attributes into SharedVersionInfo.cs at the solution level. - Move other shared assembly attributes into SharedAssemblyInfo.cs. - Leave attributes that vary by assembly in the project-specific AssemblyInfo.cs for each project. - Link these shared files from all projects. - The values in several of these attributes are incorrect and will be cleaned up later. - Now we can change things like the AssemblyVersion in one place.

---
+++ SharedAssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+
+
+// TODO : Set the company.
+[assembly: AssemblyCompany("")]
+
+// TODO : AssemblyProduct should be set here too.
+
+// TODO : Fix the copyright and trademark info.
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+
+// TODO : Set to the proper configuration as per build env.
+[assembly: AssemblyConfiguration("")]
+
+[assembly: AssemblyCulture("")]
