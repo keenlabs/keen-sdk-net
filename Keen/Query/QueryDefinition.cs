9e59776ad37adfcad28de63066b78f33f71980ce
Sha <shale.kuzmanovski@clickview.com.au>
2017-07-26 10:26:02 +1000

    Add a query definition model

---
+++ Keen/Query/QueryDefinition.cs
@@ -0,0 +1,16 @@
+﻿namespace Keen.Core.Query
+{
+    using System.Collections.Generic;
+
+    public class QueryDefinition
+    {
+        public string ProjectId { get; set; }
+        public string AnalysisType { get; set; }
+        public string EventCollection { get; set; }
+        public IEnumerable<QueryFilter> Filters { get; set; }
+        public string Timeframe { get; set; }
+        public string Timezone { get; set; }
+        public string Interval { get; set; }
+        public IEnumerable<string> GroupBy { get; set; }
+    }
+}
