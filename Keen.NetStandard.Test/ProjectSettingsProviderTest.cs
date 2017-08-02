3f206e3c2c6c93ae710468aec988cb4cc9fddf6c
Brian Baumhover <b.baumhover@gmail.com>
2017-08-02 16:06:08 -0500

    Remove code that isn't needed. Environment variables only affect the running process' environment, so there's no point in saving and restoring them.

--- Keen.NetStandard.Test/ProjectSettingsProviderTest.cs
+++ Keen.NetStandard.Test/ProjectSettingsProviderTest.cs
@@ -22,55 +22,25 @@ namespace Keen.NetStandard.Tests
         [Test]
         public void SettingsProviderEnv_VarsNotSet_Throws()
         {
-            var ProjectId = Environment.GetEnvironmentVariable(KeenConstants.KeenProjectId);
-            var MasterKey = Environment.GetEnvironmentVariable(KeenConstants.KeenMasterKey);
-            var WriteKey = Environment.GetEnvironmentVariable(KeenConstants.KeenWriteKey);
-            var ReadKey = Environment.GetEnvironmentVariable(KeenConstants.KeenReadKey);
+            Environment.SetEnvironmentVariable(KeenConstants.KeenProjectId, null);
+            Environment.SetEnvironmentVariable(KeenConstants.KeenMasterKey, null);
+            Environment.SetEnvironmentVariable(KeenConstants.KeenWriteKey, null);
+            Environment.SetEnvironmentVariable(KeenConstants.KeenReadKey, null);
 
-            try
-            {
-                Environment.SetEnvironmentVariable(KeenConstants.KeenProjectId, null);
-                Environment.SetEnvironmentVariable(KeenConstants.KeenMasterKey, null);
-                Environment.SetEnvironmentVariable(KeenConstants.KeenWriteKey, null);
-                Environment.SetEnvironmentVariable(KeenConstants.KeenReadKey, null);
-
-                var settings = new ProjectSettingsProviderEnv();
-                Assert.Throws<KeenException>(() => new KeenClient(settings));
-            }
-            finally
-            {
-                Environment.SetEnvironmentVariable(KeenConstants.KeenProjectId, ProjectId);
-                Environment.SetEnvironmentVariable(KeenConstants.KeenMasterKey, MasterKey);
-                Environment.SetEnvironmentVariable(KeenConstants.KeenWriteKey, WriteKey);
-                Environment.SetEnvironmentVariable(KeenConstants.KeenReadKey, ReadKey);
-            }
+            var settings = new ProjectSettingsProviderEnv();
+            Assert.Throws<KeenException>(() => new KeenClient(settings));
         }
 
         [Test]
         public void SettingsProviderEnv_VarsSet_Success()
         {
-            var ProjectId = Environment.GetEnvironmentVariable(KeenConstants.KeenProjectId);
-            var MasterKey = Environment.GetEnvironmentVariable(KeenConstants.KeenMasterKey);
-            var WriteKey = Environment.GetEnvironmentVariable(KeenConstants.KeenWriteKey);
-            var ReadKey = Environment.GetEnvironmentVariable(KeenConstants.KeenReadKey);
+            Environment.SetEnvironmentVariable(KeenConstants.KeenProjectId, "X");
+            Environment.SetEnvironmentVariable(KeenConstants.KeenMasterKey, "X");
+            Environment.SetEnvironmentVariable(KeenConstants.KeenWriteKey, "X");
+            Environment.SetEnvironmentVariable(KeenConstants.KeenReadKey, "X");
 
-            try
-            {
-                Environment.SetEnvironmentVariable(KeenConstants.KeenProjectId, "X");
-                Environment.SetEnvironmentVariable(KeenConstants.KeenMasterKey, "X");
-                Environment.SetEnvironmentVariable(KeenConstants.KeenWriteKey, "X");
-                Environment.SetEnvironmentVariable(KeenConstants.KeenReadKey, "X");
-
-                var settings = new ProjectSettingsProviderEnv();
-                Assert.DoesNotThrow(() => new KeenClient(settings));
-            }
-            finally
-            {
-                Environment.SetEnvironmentVariable(KeenConstants.KeenProjectId, ProjectId);
-                Environment.SetEnvironmentVariable(KeenConstants.KeenMasterKey, MasterKey);
-                Environment.SetEnvironmentVariable(KeenConstants.KeenWriteKey, WriteKey);
-                Environment.SetEnvironmentVariable(KeenConstants.KeenReadKey, ReadKey);
-            }
+            var settings = new ProjectSettingsProviderEnv();
+            Assert.DoesNotThrow(() => new KeenClient(settings));
         }
     }
 }
