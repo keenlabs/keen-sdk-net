484e553b1adbb91a7e8885aa2efa0f9bb86f0a92
Brian Baumhover <b.baumhover@gmail.com>
2017-08-02 19:30:23 -0500

    Copy existing EventCacheTest.cs, EventMock.cs, and TestBase from KeenClientTest.cs

---
+++ Keen.NetStandard.Test/TestBase.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using NUnit.Framework;
+
+
+namespace Keen.NetStandard.Test
+{
+    public class TestBase
+    {
+        public static bool UseMocks = true;
+        public IProjectSettings SettingsEnv;
+
+        [OneTimeSetUp]
+        public virtual void Setup()
+        {
+            if (UseMocks)
+                SetupEnv();
+            SettingsEnv = new ProjectSettingsProviderEnv();
+        }
+
+        [OneTimeTearDown]
+        public virtual void TearDown()
+        {
+            if (UseMocks)
+                ResetEnv();
+        }
+
+        public static void SetupEnv()
+        {
+            foreach (var s in new[] { "KEEN_PROJECT_ID", "KEEN_MASTER_KEY", "KEEN_WRITE_KEY", "KEEN_READ_KEY" })
+                Environment.SetEnvironmentVariable(s, "0123456789ABCDEF0123456789ABCDEF");
+        }
+
+        public static void ResetEnv()
+        {
+            foreach (var s in new[] { "KEEN_PROJECT_ID", "KEEN_MASTER_KEY", "KEEN_WRITE_KEY", "KEEN_READ_KEY" })
+                Environment.SetEnvironmentVariable(s, null);
+        }
+    }
+}
