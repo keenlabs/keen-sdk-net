4a5b56f4cb0ce33ad0b0b9a49b290cbe4ecccd0a
Sha <shale.kuzmanovski@clickview.com.au>
2017-08-11 17:18:59 +1000

    Provide XML comments for the dataset classes exposed via KeenClient

--- Keen/Query/QueryDefinition.cs
+++ Keen/Query/QueryDefinition.cs
@@ -2,15 +2,42 @@
 {
     using System.Collections.Generic;
 
+    /// <summary>
+    /// Holds information describing the query that is cached within a cached dataset.
+    /// </summary>
     public class QueryDefinition
     {
+        /// <summary>
+        /// Unique id of the project to analyze.
+        /// </summary>
         public string ProjectId { get; set; }
+        /// <summary>
+        /// The type of analysis for this query (e.g. count, count_unique, sum etc.)
+        /// </summary>
         public string AnalysisType { get; set; }
+        /// <summary>
+        /// Specifies the name of the event collection to analyze.
+        /// </summary>
         public string EventCollection { get; set; }
+        /// <summary>
+        /// Refines the scope of events to be included in the analysis based on event property values.
+        /// </summary>
         public IEnumerable<QueryFilter> Filters { get; set; }
+        /// <summary>
+        /// Limits analysis to a specific period of time when the events occurred.
+        /// </summary>
         public string Timeframe { get; set; }
+        /// <summary>
+        /// Assigns a timezone offset to relative timeframes.
+        /// </summary>
         public string Timezone { get; set; }
+        /// <summary>
+        /// Specifies the size of time interval by which to group results. Using this parameter changes the response format.
+        /// </summary>
         public string Interval { get; set; }
+        /// <summary>
+        /// Specifies the name of a property by which to group results. Using this parameter changes the response format.
+        /// </summary>
         public IEnumerable<string> GroupBy { get; set; }
     }
 }
