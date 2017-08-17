3fcb789ddd383b4298a9b593623cdabf72689355
Sha <shale.kuzmanovski@clickview.com.au>
2017-08-17 16:55:09 +1000

    Add some simple tests for the new dataset methods

--- Keen.NET.Test/DatasetTests.cs
+++ Keen.NET.Test/DatasetTests.cs
@@ -1,5 +1,6 @@
 ﻿namespace Keen.Net.Test
 {
+    using System.Collections.Generic;
     using System.Threading.Tasks;
     using Core;
     using Core.Dataset;
@@ -13,6 +14,7 @@
     {
         private const string _datasetName = "video-view";
         private const string _indexBy = "12";
+        private const int _listDatasetLimit = 1;
 
         [Test]
         public async Task GetDatasetResults_Success()
@@ -39,6 +41,128 @@
 
             var dataset = await client.QueryDatasetAsync(_datasetName, _indexBy, timeframe.ToString());
             Assert.IsNotNull(dataset);
+
+            datasetMock?.VerifyAll();
+        }
+
+        [Test]
+        public async Task GetDatasetDefinition_Success()
+        {
+            var result = new DatasetDefinition();
+
+            var client = new KeenClient(SettingsEnv);
+
+            Mock<IDataset> datasetMock = null;
+
+            if (UseMocks)
+            {
+                datasetMock = new Mock<IDataset>();
+                datasetMock.Setup(m => m.Definition(
+                        It.Is<string>(n => n == _datasetName)))
+                    .Returns(Task.FromResult(result));
+
+                client.Datasets = datasetMock.Object;
+            }
+
+            var datasetDefinition = await client.GetDatasetDefinitionAsync(_datasetName);
+            Assert.IsNotNull(datasetDefinition);
+
+            datasetMock?.VerifyAll();
+        }
+
+        [Test]
+        public async Task ListDatasetDefinitions_Success()
+        {
+            var result = new DatasetDefinitionCollection();
+
+            var client = new KeenClient(SettingsEnv);
+
+            Mock<IDataset> datasetMock = null;
+
+            if (UseMocks)
+            {
+                datasetMock = new Mock<IDataset>();
+                datasetMock.Setup(m => m.ListDefinitions(
+                        It.Is<int>(n => n == _listDatasetLimit),
+                        It.Is<string>(n => n == _datasetName)))
+                    .Returns(Task.FromResult(result));
+
+                client.Datasets = datasetMock.Object;
+            }
+
+            var datasetDefinitionCollection = await client.ListDatasetDefinitionsAsync(_listDatasetLimit, _datasetName);
+            Assert.IsNotNull(datasetDefinitionCollection);
+
+            datasetMock?.VerifyAll();
+        }
+
+        [Test]
+        public async Task ListDatasetAllDefinitions_Success()
+        {
+            IEnumerable<DatasetDefinition> result = new List<DatasetDefinition>();
+
+            var client = new KeenClient(SettingsEnv);
+
+            Mock<IDataset> datasetMock = null;
+
+            if (UseMocks)
+            {
+                datasetMock = new Mock<IDataset>();
+                datasetMock.Setup(m => m.ListAllDefinitions())
+                    .Returns(Task.FromResult(result));
+
+                client.Datasets = datasetMock.Object;
+            }
+
+            var datasetDefinitions = await client.ListAllDatasetDefinitionsAsync();
+            Assert.IsNotNull(datasetDefinitions);
+
+            datasetMock?.VerifyAll();
+        }
+
+        [Test]
+        public async Task CreateDataset_Success()
+        {
+            var result = new DatasetDefinition();
+
+            var client = new KeenClient(SettingsEnv);
+
+            Mock<IDataset> datasetMock = null;
+
+            if (UseMocks)
+            {
+                datasetMock = new Mock<IDataset>();
+                datasetMock.Setup(m => m.CreateDataset(
+                        It.Is<DatasetDefinition>(n => n != null)))
+                    .Returns(Task.FromResult(result));
+
+                client.Datasets = datasetMock.Object;
+            }
+
+            var datasetDefinition = await client.CreateDatasetAsync(new DatasetDefinition());
+            Assert.IsNotNull(datasetDefinition);
+
+            datasetMock?.VerifyAll();
+        }
+
+        [Test]
+        public async Task DeleteDataset_Success()
+        {
+            var client = new KeenClient(SettingsEnv);
+
+            Mock<IDataset> datasetMock = null;
+
+            if (UseMocks)
+            {
+                datasetMock = new Mock<IDataset>();
+                datasetMock.Setup(m => m.DeleteDataset(
+                        It.Is<string>(n => n == _datasetName)))
+                    .Returns(Task.Delay(0));
+
+                client.Datasets = datasetMock.Object;
+            }
+
+            await client.DeleteDatasetAsync(_datasetName);
 
             datasetMock?.VerifyAll();
         }
