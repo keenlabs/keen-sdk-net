c2602e99a236e0190fa16d3a9126f13df53536b2
Sha <shale.kuzmanovski@clickview.com.au>
2017-08-17 16:21:48 +1000

    Remove models which are not used

--- Keen/Dataset/DatasetMetadata.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Keen.Core.Dataset
-{
-    public class DatasetMetadata
-    {
-        public DatasetDefinition Dataset { get; set; }
-        public string Request { get; set; }
-    }
-}
