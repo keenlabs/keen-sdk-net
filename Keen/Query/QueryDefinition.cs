c39aa24134cc40c39d77c0b4e6bd99d66fe63dec
Sha <shale.kuzmanovski@clickview.com.au>
2017-08-17 15:24:11 +1000

    Added Validation and Serialization utility for QueryDefinition

--- Keen/Query/QueryDefinition.cs
+++ Keen/Query/QueryDefinition.cs
@@ -2,6 +2,12 @@
 
 namespace Keen.Core.Query
 {
+    using System;
+    using System.Linq;
+    using Dataset;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Linq;
+
     /// <summary>
     /// Holds information describing the query that is cached within a cached dataset.
     /// </summary>
@@ -39,5 +45,76 @@ namespace Keen.Core.Query
         /// Specifies the name of a property by which to group results. Using this parameter changes the response format.
         /// </summary>
         public IEnumerable<string> GroupBy { get; set; }
+    }
+
+    internal static class QueryDefinitionExtensions
+    {
+        public static void ValidateForCachedDataset(this QueryDefinition query)
+        {
+            if (string.IsNullOrWhiteSpace(query.AnalysisType))
+            {
+                throw new KeenException("QueryDefinition must have an analysis type");
+            }
+
+            if (string.IsNullOrWhiteSpace(query.EventCollection))
+            {
+                throw new KeenException("QueryDefinition must specify an event collection");
+            }
+
+            if (string.IsNullOrWhiteSpace(query.Timeframe))
+            {
+                throw new KeenException("QueryDefinition must specify a timeframe");
+            }
+
+            if (string.IsNullOrWhiteSpace(query.Interval))
+            {
+                throw new KeenException("QueryDefinition must specify an interval");
+            }
+        }
+    }
+
+    /*
+     * This is used because the PUT endpoint for a dataset take a string for group_by, but return an array of strings.
+     */
+    internal class QueryDefinitionConverter : JsonConverter
+    {
+        /* This prevents JToken.ToObject form recursively calling ReadJson until the stack runs out */
+        private bool _isNested;
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            _isNested = true;
+            var query = JToken.FromObject(value, serializer);
+            var groupByList = query["group_by"]?.ToArray();
+
+            if (groupByList != null)
+            {
+                query["group_by"] = groupByList.FirstOrDefault()?.ToString();
+            }
+            
+            serializer.Serialize(writer, query);
+            _isNested = false;
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            var token = JToken.Load(reader);
+            if (token.Type != JTokenType.Object)
+                return null;
+
+            _isNested = true;
+            var query = token.ToObject<QueryDefinition>(serializer);
+            _isNested = false;
+
+            return query;
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            if (_isNested)
+                return false;
+
+            return objectType == typeof(QueryDefinition);
+        }
     }
 }
