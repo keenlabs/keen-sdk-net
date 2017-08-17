0ed3fb23cd25842c0a0fa5e022ccbcc7cb49527c
Sha <shale.kuzmanovski@clickview.com.au>
2017-08-17 15:23:07 +1000

    Added class for fetching lists of dataset definitions

---
+++ Keen/Dataset/DatasetDefinitionCollection.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Keen.Core.Dataset
+{
+    public class DatasetDefinitionCollection
+    {
+        public IEnumerable<DatasetDefinition> Datasets { get; set; }
+        public string NextPageUrl { get; set; }
+        public int Count { get; set; }
+    }
+}
