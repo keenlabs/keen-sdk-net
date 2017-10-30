32a8394521fb06e6e2654f0b12253b83e363ed9b
Isham Mohamed <isham4you@gmail.com>
2017-10-30 13:47:48 +0530

    Added some changes as per the comments from masojus

--- Keen/AccessKey/AccessKey.cs
+++ Keen/AccessKey/AccessKey.cs
@@ -10,40 +10,39 @@ namespace Keen.Core.AccessKey
     {
         public string Name { get; set; }
         public bool IsActive { get; set; }
-        public List<string> Permitted { get; set; }
+        public IEnumerable<string> Permitted { get; set; }
         public string Key { get; set; }
-        public string ProjectId { get; set; }
         public Options Options { get; set; }
     }
 
     public class SavedQueries
     {
-        public string[] Blocked { get; set; }
+        public IEnumerable<string> Blocked { get; set; }
         public IEnumerable<QueryFilter> Filters { get; set; }
-        public string[] Allowed { get; set; }
+        public IEnumerable<string> Allowed { get; set; }
     }
 
-    public class Autofill
+    public class Quaries
     {
-        public IEnumerable<IDictionary<string, string>> Filters { get; set; }
+        public IEnumerable<QueryFilter> Filters { get; set; }
     }
 
     public class Writes
     {
-        public dynamic autofill { get; set; } // "customer": { "id": "93iskds39kd93id", "name": "Ada Corp." }
+        public dynamic Autofill { get; set; } // "customer": { "id": "93iskds39kd93id", "name": "Ada Corp." }
     }
 
     public class Datasets
     {
-        public string[] Operations { get; set; }
-        public IEnumerable<IDictionary<string, string>> Allowed { get; set; } //may be this also dynamic?
-        public string[] Blocked { get; set; }
+        public IEnumerable<string> Operations { get; set; }
+        public IDictionary<string, IDictionary<string, IDictionary<string, string>>> Allowed { get; set; }
+        public IEnumerable<string> Blocked { get; set; }
     }
 
     public class CachedQueries
     {
-        public string[] Blocked { get; set; }
-        public string[] Allowed { get; set; }
+        public IEnumerable<string> Blocked { get; set; }
+        public IEnumerable<string> Allowed { get; set; }
     }
 
     public class Options
@@ -52,6 +51,6 @@ namespace Keen.Core.AccessKey
         public Writes Writes { get; set; }
         public Datasets Datasets { get; set; }
         public CachedQueries CachedQueries { get; set; }
-        public IQueries Queries { get; set; }
+        public Quaries Queries { get; set; }
     }
 }
